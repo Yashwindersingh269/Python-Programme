6ba82ba42d23b1115000d2e9d66367a3cb693145
Yashwindersingh269 <yashwindersingh2175@gmail.com>
2025-07-21 16:58:10 +0530

    Add files via upload

---
+++ 85_ Multithreading.py
@@ -0,0 +1,17 @@
+import threading
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+# Indicates some task being done
+def func(seconds):
+  print(f"Sleeping for {seconds} seconds")
+  time.sleep(seconds)
+  return seconds
+
+def main():
+  time1 = time.perf_counter()
+  # Normal Code
+  # func(4) 
+  # func(2)
+  # func(1)
+  
